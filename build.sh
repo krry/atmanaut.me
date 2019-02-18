5c85e9f5a21dcd386591bd2bab726be77f1e8933
Kerry Snyder <krry@me.com>
2019-02-18 13:23:32 -0600

    use hugo extended in build for sass

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 yum install -y wget
 
 HUGO_VERSION=0.54.0
-HUGO_TAR=hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+HUGO_TAR=hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
 HUGO_DL=https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_TAR}
 
 wget ${HUGO_DL}
