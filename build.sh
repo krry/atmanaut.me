3f771082f027959e952560a6f57929e55b78a0bf
Kerry Snyder <krry@me.com>
2019-02-18 15:55:41 -0600

    install theme dependencies for PostCSS during build

--- build.sh
+++ build.sh
@@ -5,9 +5,12 @@ yum install -y wget
 HUGO_VERSION=0.54.0
 HUGO_TAR=hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 HUGO_DL=https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_TAR}
-
 wget ${HUGO_DL}
 tar -xvzf hugo*.tar.gz -C /usr/local/bin
 git clone https://github.com/krry/chakra-hugo-theme.git themes/chakra
 npm install postcss-cli
+echo $PWD
+cd themes/chakra
+npm install
+cd ../..
 hugo -d public
