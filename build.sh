efe5bf2ccfbba07ba9c8f435ed46e96b1f808c9a
Kerry Snyder <krry@me.com>
2019-03-29 02:06:12 -0500

    keep homepage template here, simplify now build

--- build.sh
+++ build.sh
@@ -8,9 +8,4 @@ HUGO_DL=https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HU
 wget ${HUGO_DL}
 tar -xvzf hugo*.tar.gz -C /usr/local/bin
 git clone https://github.com/krry/chakra-hugo-theme.git themes/chakra
-npm install postcss-cli
-echo "$PWD"
-cd themes/chakra || return
-npm install
-cd ../.. || return
 hugo -d public
